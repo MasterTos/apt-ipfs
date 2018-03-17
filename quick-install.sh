67bdb265d2fa146e50b3d6c5d7c6cc12eb4927e6
MasterTos <MasterTos@yahoo.com>
2018-03-17 20:54:16 +0700

    changes name

--- quick-install.sh
+++ quick-install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 
-apt_fast_installation() {
+apt_ipfs_installation() {
   if ! dpkg-query --show aria2 >/dev/null 2>&1; then
     sudo apt-get update
     sudo apt-get install -y aria2
@@ -16,10 +16,10 @@ apt_fast_installation() {
 
 
 if [[ "$EUID" -eq 0 ]]; then
-  apt_fast_installation
+  apt_ipfs_installation
 else
   type sudo >/dev/null 2>&1 || { echo "sudo not installed, change into root context" >&2; exit 1; }
 
-  DECL="$(declare -f apt_fast_installation)"
-  sudo bash -c "$DECL; apt_fast_installation"
+  DECL="$(declare -f apt_ipfs_installation)"
+  sudo bash -c "$DECL; apt_ipfs_installation"
 fi
